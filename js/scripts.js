df2dbca02f5baa75b30d0cac2778d3ab26806c46
JISSIN L <fobven@gmail.com>
2019-02-11 23:46:01 +0800

    Update scripts.js
    add resume link

--- js/scripts.js
+++ js/scripts.js
@@ -17,9 +17,9 @@ window.onload = function() {
 
   var messages = [
     'Hey there 👋',
-    'I\'m Jissin from China 🇨🇳',
-    'I write JavaScript',
-    'I\'m currently accepting a job.<br> You can contact me at <a href="mailto:fobven@gmail.com">fobven@gmail.com</a>',
+    'I\'m Jissin',
+    'I like coding and designing',
+    'I\'m currently accepting a job<br> Click <a target="_blank" href="http://kwl.im/resume.html">here</a> to watch my resume<br> You can contact me at <a href="mailto:fobven@gmail.com">fobven@gmail.com</a>',
     '<a target="_blank" href="https://twitter.com/fobven">twitter.com/fobven</a><br><a target="_blank" href="https://github.com/fobven">github.com/fobven</a><br><a target="_blank" href="https://blog.kwl.im">blog.kwl.im</a>',
     getCurrentTime(),
     '✍️ J.'
